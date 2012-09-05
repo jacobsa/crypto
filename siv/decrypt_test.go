d5abfde123492fe0435dbe338978de60a7bd9785
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-09-05 11:20:17 +1000

    DecryptTest.CorruptedAssociatedData

--- siv/decrypt_test.go
+++ siv/decrypt_test.go
@@ -188,7 +188,21 @@ func (t *DecryptTest) CorruptedCiphertext() {
 }
 
 func (t *DecryptTest) CorruptedAssociatedData() {
-	ExpectEq("TODO", "")
+	// Grab a test case.
+	cases := aes_testing.EncryptCases()
+	AssertGt(len(cases), 10)
+	c := cases[10]
+
+	// Corrupt its associated data and call.
+	AssertGt(len(c.Associated), 2)
+	AssertGt(len(c.Associated[2]), 3)
+	c.Associated[2][3]++
+
+	_, err := siv.Decrypt(c.Key, c.Output, c.Associated)
+	ExpectThat(err, HasSubstr("authentic"))
+
+	_, ok := err.(siv.NotAuthenticError)
+	ExpectTrue(ok, "Not an instance of NotAuthenticError.")
 }
 
 func (t *DecryptTest) Rfc5297TestCaseA1() {
