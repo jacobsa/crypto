8d51488f1e290eaa0c3cee55b04e37e649910498
Michael Hudson-Doyle <michael.hudson@canonical.com>
2016-11-10 08:05:36 +1300

    Add build tags for other 64 bit architectures

--- cmac/hash_64bit.go
+++ cmac/hash_64bit.go
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build amd64 arm64
+// +build amd64 arm64 ppc64 ppc64le s390x mips64 mips64le
 
 package cmac
 
